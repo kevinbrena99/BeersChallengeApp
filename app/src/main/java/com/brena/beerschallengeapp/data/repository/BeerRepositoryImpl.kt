459ae8a86c244e5f3f3e823954925e11fcfd980d
Kevin Breña <kevin.brena99@gmail.com>
2021-06-04 00:13:56 -0500

    Add: BeerRepository.kt and implementation

---
+++ app/src/main/java/com/brena/beerschallengeapp/data/repository/BeerRepositoryImpl.kt
@@ -0,0 +1,12 @@
+package com.brena.beerschallengeapp.data.repository
+
+import com.brena.beerschallengeapp.domain.repository.BeerRepository
+
+/**
+ * Created by Kevin Breña on 4/06/2021.
+ * kevin.brena99@gmail.com
+ *
+ * Lima, Peru.
+ **/
+class BeerRepositoryImpl: BeerRepository {
+}
---
+++ app/src/main/java/com/brena/beerschallengeapp/domain/repository/BeerRepository.kt
@@ -0,0 +1,10 @@
+package com.brena.beerschallengeapp.domain.repository
+
+/**
+ * Created by Kevin Breña on 4/06/2021.
+ * kevin.brena99@gmail.com
+ *
+ * Lima, Peru.
+ **/
+interface BeerRepository {
+}
